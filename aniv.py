d7c1094675d359dcf42d9d0877002b6b42277c48
Mariana Marins Sant' Anna <48518135+MarianaMarins@users.noreply.github.com>
2021-09-09 18:06:42 -0300

    Update aniv.py

--- aniv.py
+++ aniv.py
@@ -14,14 +14,14 @@ nome = st.text_input ("Como as pessoas te chamam: ")
 #nasci = nasci.split("/")
 btn = st.button('Calcular o aniversário')
 if btn('Gerar número aleatório'):
-if dia[0] == nasci.day and dia[1] == nasci.month:
-    idade = int(dia[2]) - int(nasci.year)
-    if idade == 30:
-        if nome.split(' ')[0] =='Igor' or nome.split(' ')[0] == 'Girão' or nome == 'Igor Girão':
-            print(f'Hoje é seu aniversário.\nTrintou.\n{nome},tá fazendo {idade} anos')
+    if dia[0] == nasci.day and dia[1] == nasci.month:
+        idade = int(dia[2]) - int(nasci.year)
+        if idade == 30:
+            if nome.split(' ')[0] =='Igor' or nome.split(' ')[0] == 'Girão' or nome == 'Igor Girão':
+                print(f'Hoje é seu aniversário.\nTrintou.\n{nome},tá fazendo {idade} anos')
+            else:
+                print('Hoje é até seu aniversário mas não é meu nerdzila.')
         else:
-            print('Hoje é até seu aniversário mas não é meu nerdzila.')
+            print('Hoje é até seu aniversário porém não está fazendo trinta anos e nem é meu nerdzila')
     else:
-        print('Hoje é até seu aniversário porém não está fazendo trinta anos e nem é meu nerdzila')
-else:
-    print("Hoje não é seu aniversário,querido(a).")
+        print("Hoje não é seu aniversário,querido(a).")
