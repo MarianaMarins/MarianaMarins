a40a8d281c90ed98a663422f3ed76987bac557c5
Mariana Marins Sant' Anna <48518135+MarianaMarins@users.noreply.github.com>
2021-09-09 18:12:02 -0300

    Update aniv.py

--- aniv.py
+++ aniv.py
@@ -13,7 +13,7 @@ nasci = st.date_input("Data do seu nascimento: ")
 nome = st.text_input ("Como as pessoas te chamam: ")
 #nasci = nasci.split("/")
 btn = st.button('Calcular o aniversário')
-if btn('Calcular o aniversário'):
+if btn:
     if dia[0] == nasci.day and dia[1] == nasci.month:
         idade = int(dia[2]) - int(nasci.year)
         if idade == 30:
