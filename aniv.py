f89bf3646ee326a4af28f85eda0a2dcbaf834089
Mariana Marins Sant' Anna <48518135+MarianaMarins@users.noreply.github.com>
2021-09-09 19:45:12 -0300

    Update aniv.py

--- aniv.py
+++ aniv.py
@@ -7,15 +7,15 @@ st.markdown(" #Seráquetrintou?")
 
 
 hoje = datetime.date.today() 
-dia = hoje.split('-')
+#dia = hoje.split('-')
 
 nasci = st.date_input("Data do seu nascimento: ", min_value = datetime.date(1980, 1, 1))
 nome = st.text_input ("Como as pessoas te chamam: ")
 #nasci = nasci.split("/")
 btn = st.button('Calcular o aniversário')
 if btn:
-    if dia[0] == nasci.day and dia[1] == nasci.month:
-        idade = int(dia[2]) - int(nasci.year)
+    if dia.day == nasci.day and dia.day == nasci.month:
+        idade = int(dia.year) - int(nasci.year)
         if idade == 30:
             if nome.split(' ')[0] =='Igor' or nome.split(' ')[0] == 'Girão' or nome == 'Igor Girão':
                 st.write(f'Hoje é seu aniversário.\nTrintou.\n{nome},tá fazendo {idade} anos')
